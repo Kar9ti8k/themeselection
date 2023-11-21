dd162a3932d1e519cc9df61bb0655099a88d23e0
Shravya Sankineni <shravya.sankineni@quinterrasoft.com>
2023-11-21 22:50:46 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import './App.css'
+import Box from '@mui/material/Box'
+import {
+  TextField,
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  Checkbox,
+  Button,
+} from '@mui/material'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Card sx={{ maxWidth: 500, backgroundColor: '#fff', height: '84vh' }}>
+        <CardContent>
+          <Box>
+            <Box>
+              <Typography variant='h5' component='h2'>
+                MATERIO
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant='h5' component='h2'>
+                Welcome to Materio! 👋🏻
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                color='text.secondary'
+                sx={{
+                  paddingLeft: 0.2,
+                  display: 'flex',
+
+                  flexDirection: 'row',
+                }}
+              >
+                Please sign-in to your account and start the adventure
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                marginTop: 4,
+                paddingLeft: 7,
+                width: 300,
+                gap: 2,
+                display: 'flex',
+
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <TextField required fullWidth label='Email' variant='outlined' />
+              <TextField
+                required
+                fullWidth
+                label='Password'
+                variant='outlined'
+              />
+            </Box>
+            <Box
+              sx={{
+                marginTop: 0.2,
+                paddingLeft: 5,
+                width: 200,
+                gap: 2,
+                display: 'flex',
+
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+            >
+              <Checkbox />
+              <Typography>Remember Me</Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+      {/* <Box sx={{}}>
+        <Typography variant="h3" component="h2">
+          h1. Heading
+        </Typography>
+        <TextField />
+      </Box> */}
+    </div>
+  )
+}
